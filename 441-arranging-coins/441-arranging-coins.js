f968378bc8e24980e6d560330c0f69829df0e4b5
mostafaac30 <57822109+mostafaac30@users.noreply.github.com>
2022-05-08 11:05:03 +0200

    Time: 107 ms (58.06%), Space: 44.3 MB (43.06%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.js
+++ 441-arranging-coins/441-arranging-coins.js
@@ -3,8 +3,25 @@
  * @return {number}
  */
 var arrangeCoins = function (n) {
-    return parseInt(Math.sqrt(2 * n + 0.25) - 0.5);
+    // return (Math.sqrt(2 * n + 0.25) - 0.5);
+    let min = 0, max = n, med, last = -1, coin;
+
+    while (max >= min) {
+        med = parseInt((max + min) / 2)
+        coin = med * (med + 1) / 2
+        if (coin == n) {
+            return med
+        }
+        else if (coin < n) {
+            min = med + 1
+        }
+        else if (coin > n) {
+            max = med - 1
+        }
+    }
+
+    return max
 
 };
 
-console.log(arrangeCoins(5))
+console.log(arrangeCoins(4))
