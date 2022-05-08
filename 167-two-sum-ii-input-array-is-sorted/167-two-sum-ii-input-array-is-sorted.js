6a156d013aca951855ee8048894198f8afb63d48
mostafaac30 <57822109+mostafaac30@users.noreply.github.com>
2022-05-09 01:40:18 +0200

    Time: 76 ms (65.95%), Space: 43 MB (56.63%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -4,23 +4,32 @@
  * @return {number[]}
  */
 var twoSum = function (numbers, target) {
-    let min, max, mid, comp = 0;
-    for (let i = 0; i < numbers.length; i++) {
-        comp = target - numbers[i];
-        min = i + 1; max = numbers.length - 1;
-        while (max >= min) {
-            mid = parseInt((max + min) / 2);
-            if (numbers[mid] == comp) {
-                return [i + 1, mid + 1]
-            }
-            else if (numbers[mid] < comp) {
-                min = mid + 1;
-            }
-            else {
-                max = mid - 1;
-            }
-        }
+    let i = 0, j = numbers.length - 1, len = numbers.length;
+    while (len--) {
+        let res = numbers[i] + numbers[j];
+        if (res == target) return [i + 1, j + 1]
+        else if (res < target) i++;
+        else j--
     }
+
+    /// Binary search
+    // let min, max, mid, comp = 0;
+    // for (let i = 0; i < numbers.length; i++) {
+    //     comp = target - numbers[i];
+    //     min = i + 1; max = numbers.length - 1;
+    //     while (max >= min) {
+    //         mid = parseInt((max + min) / 2);
+    //         if (numbers[mid] == comp) {
+    //             return [i + 1, mid + 1]
+    //         }
+    //         else if (numbers[mid] < comp) {
+    //             min = mid + 1;
+    //         }
+    //         else {
+    //             max = mid - 1;
+    //         }
+    //     }
+    // }
 };
 
-console.log(twoSum([2, 3, 4], 6))
+console.log(twoSum([0, 0, 3, 4], 0))
