7bd605fb1e38729dfaa8d876e79e0318076332f0
mostafaac30 <57822109+mostafaac30@users.noreply.github.com>
2022-05-11 14:05:20 +0200

    Time: 68 ms (79.31%), Space: 42.1 MB (73.08%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.js
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.js
@@ -6,10 +6,26 @@
  * @return {number}
  */
 var findKthPositive = function (arr, k) {
-    arr.forEach(item => {
-        if (item <= k) {
-            k++
+    //binary search 
+    let min = 0, max = arr.length - 1, med;
+    while (max >= min) {
+        med = parseInt((max + min) / 2);
+        if (arr[med] - med - 1 < k) {
+            min = med + 1
         }
-    })
-    return k
+        else {
+            max = med - 1
+        }
+    }
+    return k + min
+    
+    //O(N) method
+    // arr.forEach(item => {
+    //     if (item <= k) {
+    //         k++
+    //     }
+    // })
+    // return k
 };
+
+console.log(findKthPositive([1, 2, 3, 4], 2))
