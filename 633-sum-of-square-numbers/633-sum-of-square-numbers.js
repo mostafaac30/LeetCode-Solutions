47340550939c2a32ce0419478121c3c14afb553d
mostafaac30 <57822109+mostafaac30@users.noreply.github.com>
2022-05-11 11:24:19 +0200

    Time: 397 ms (6.81%), Space: 48 MB (11.91%) - LeetHub

--- 633-sum-of-square-numbers/633-sum-of-square-numbers.js
+++ 633-sum-of-square-numbers/633-sum-of-square-numbers.js
@@ -5,12 +5,29 @@
 var judgeSquareSum = function (c) {
     let a = 0
     while (a * a <= c) {
-        let isB = Math.sqrt(c - a * a)
-        if (isB == Math.round(isB))
+
+        // let isB = Math.sqrt(c - a * a)
+        // console.log(a, isB)
+
+        if (isPerfectSquare(c - a * a)) {
             return true
-        a++
+        } a++
+    }
+    return false
+};
+var isPerfectSquare = function (num) {
+    let min = 0, max = num, mid;
+
+    while (min <= max) {
+        mid = parseInt((max + min) / 2);
+        if (mid * mid == num) {
+            return true
+        }
+        else if (mid * mid < num) min = mid + 1;
+        else max = mid - 1;
+
     }
     return false
 };
 
-console.log(judgeSquareSum(2147482647))
+console.log(judgeSquareSum(5))
