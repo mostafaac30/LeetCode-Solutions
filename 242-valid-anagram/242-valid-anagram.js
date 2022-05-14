3301cc8dc7d52c8aced0f3eb66b3a843a400a57f
Mostafa Mahmoud <57822109+mostafaac30@users.noreply.github.com>
2022-05-14 08:38:20 +0200

    Time: 85 ms (83.63%), Space: 43.3 MB (75.57%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,17 +4,21 @@
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
-    let mp = new Map()
+    let mpS = {}
+    let mpT = {}
     if (s.length != t.length) return false
 
-    s = s.split('').sort().toString()
-    t = t.split('').sort().toString()
     let len = s.length
     while (len--) {
-        if (s.charAt(len) != t.charAt(len)) return false
+        s[len] in mpS ? mpS[s[len]]++ : mpS[s[len]] = 1
+        t[len] in mpT ? mpT[t[len]]++ : mpT[t[len]] = 1
     }
-    return t
+    len = s.length
+    for (let e in mpS) {
+        if (mpS[e] != mpT[e]) return false
+    }
+    return true
 };
 
-console.log(isAnagram("rat",
-    "car"))
+console.log(isAnagram("anagram",
+    "nagaram"))
