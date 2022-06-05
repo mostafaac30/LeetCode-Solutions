109fa113770273946f7fed769a8a58740a0724ab
Mostafa Mahmoud <57822109+mostafaac30@users.noreply.github.com>
2022-06-05 21:33:13 +0200

    Time: 9 ms (72.37%), Space: 18.8 MB (56.67%) - LeetHub

---
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        //note proplem here considers when parient node only exists as a level 1 not 0
+        if(root == NULL) return 0;
+        return 1+max(maxDepth(root->left),maxDepth(root->right));
+    }
+};
